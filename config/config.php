466f41826ba427117ae8b2079941ba8298bdc6b3
sadektouati <bilal2dellys@hotmail.com>
2023-05-19 16:11:22 -0400

    finaliser le tp

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+// URL de base du serveur
+const BASE_SERVEUR = 'http://localhost/nestor/';
+
+// Config de la BD
+const BD_HOTE = 'localhost';
+const BD_UTIL = 'root';
+const BD_MDP = '';
+const BD_NOM = 'nestor';
+
+// Module par défaut
+const MODULE_DEFAUT = 'utilisateur';
